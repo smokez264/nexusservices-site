2a1f0856689db1b01f493ff756a540f2db361219
smokez264 <goudaroleplay321@gmail.com>
2026-06-06 18:30:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Nexus Services Loaded");
+function snow(){
+ const s=document.createElement('div');
+ s.innerHTML='❄';
+ s.style.position='fixed';
+ s.style.left=Math.random()*window.innerWidth+'px';
+ s.style.top='-20px';
+ s.style.opacity=Math.random();
+ s.style.color='white';
+ document.body.appendChild(s);
+ let y=-20;
+ const t=setInterval(()=>{
+   y+=2;
+   s.style.top=y+'px';
+   if(y>window.innerHeight){clearInterval(t);s.remove();}
+ },20);
+}
+setInterval(snow,250);
